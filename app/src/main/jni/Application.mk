ec0dfea7abf84b692c782203e4328aa6abd1bab5
Chi Chang <edingroot@gmail.com>
2019-02-12 13:40:39 +0800

    Update build configs

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 # Note: APP_ABI - armeabi is no longer supported by the newer ndks
 
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-14
