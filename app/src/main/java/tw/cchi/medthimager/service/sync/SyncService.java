2fba97f35101ff8cad8ea0159ceaee6d2f27375d
Chi Chang <edingroot@gmail.com>
2019-03-01 17:29:51 +0800

    Fix service not allowed to start after Android O

--- app/src/main/java/tw/cchi/medthimager/service/sync/SyncService.java
+++ app/src/main/java/tw/cchi/medthimager/service/sync/SyncService.java
@@ -1,14 +1,21 @@
 package tw.cchi.medthimager.service.sync;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.HashSet;
@@ -26,6 +33,7 @@ import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 import tw.cchi.medthimager.Config;
 import tw.cchi.medthimager.MvpApplication;
+import tw.cchi.medthimager.R;
 import tw.cchi.medthimager.di.component.DaggerServiceComponent;
 import tw.cchi.medthimager.di.component.ServiceComponent;
 import tw.cchi.medthimager.model.RecurrentRunningStatus;
@@ -38,16 +46,7 @@ import tw.cchi.medthimager.util.CommonUtils;
 import tw.cchi.medthimager.util.NetworkUtils;
 
 public class SyncService extends Service {
-    private final String TAG = Config.TAGPRE + getClass().getSimpleName();
-
-    private IBinder mBinder;
-    private NetworkStateBroadcastReceiver networkStateReceiver;
-    private final CompositeDisposable taskWorkerSubs = new CompositeDisposable();
-
     private static final Set<Class<? extends SyncTask>> syncTaskClasses = new HashSet<>();
-    private final ConcurrentHashMap<Class<? extends SyncTask>, PublishSubject<SyncTask>> taskPublishSubjects = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<Class<? extends SyncTask>, ExecutorService> taskExecutors = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<Class<? extends SyncTask>, RecurrentRunningStatus> taskRunningStatus = new ConcurrentHashMap<>();
 
     static {
         // All sync tasks should be added here
@@ -57,8 +56,20 @@ public class SyncService extends Service {
         syncTaskClasses.add(UpSyncThImagesTask.class);
     }
 
+    private final String TAG = Config.TAGPRE + getClass().getSimpleName();
+    private final CompositeDisposable taskWorkerSubs = new CompositeDisposable();
+    private final ConcurrentHashMap<Class<? extends SyncTask>, PublishSubject<SyncTask>> taskPublishSubjects = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Class<? extends SyncTask>, ExecutorService> taskExecutors = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Class<? extends SyncTask>, RecurrentRunningStatus> taskRunningStatus = new ConcurrentHashMap<>();
+    private IBinder mBinder;
+    private NetworkStateBroadcastReceiver networkStateReceiver;
+
     public static void start(Context context) {
-        context.startService(new Intent(context, SyncService.class));
+        // TODO:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            context.startForegroundService(new Intent(context, SyncService.class));
+        else
+            context.startService(new Intent(context, SyncService.class));
     }
 
     public static void stop(Context context) {
@@ -68,6 +79,8 @@ public class SyncService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startMyOwnForeground();
 
         ServiceComponent component = DaggerServiceComponent.builder()
                 .applicationComponent(((MvpApplication) getApplication()).getComponent())
@@ -78,6 +91,30 @@ public class SyncService extends Service {
         networkStateReceiver = new NetworkStateBroadcastReceiver();
         registerReceiver(networkStateReceiver,
                 new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startMyOwnForeground() {
+        String NOTIFICATION_CHANNEL_ID = "tw.cchi.medthimager";
+        String channelName = "SyncService";
+
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        channel.setLightColor(Color.BLUE);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("SyncService is running")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+
+        startForeground(2, notification);
     }
 
     @Override
@@ -97,39 +134,40 @@ public class SyncService extends Service {
             // Only one task with same class is allowed to run at the same time
             Observable<SyncTask> taskWorkerSub = taskPublishSubjects.get(syncTaskClass);
             taskWorkerSubs.add(taskWorkerSub
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .subscribe(
-                    syncTask -> {
-                        taskRunningStatus.get(syncTask.getClass()).setRunning(true);
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.io())
+                    .subscribe(
+                            syncTask -> {
+                                taskRunningStatus.get(syncTask.getClass()).setRunning(true);
 
-                        syncTask.setSyncService(this);
-                        Future<Void> future = taskExecutors.get(syncTask.getClass()).submit(syncTask);
-                        try {
-                            Log.i(TAG, "timeout=" + syncTask.getTimeout());
-                            if (syncTask.getTimeout() != 0)
-                                future.get(syncTask.getTimeout(), TimeUnit.MILLISECONDS);
-                            else
-                                future.get();
-                        } catch (Exception e) {
-                            if (e instanceof TimeoutException) {
-                                Log.w(TAG, "Timeout, canceling task: " + syncTask.getClass().getSimpleName());
-                                syncTask.dispose();
+                                syncTask.setSyncService(this);
+                                Future<Void> future = taskExecutors.get(syncTask.getClass()).submit(syncTask);
+                                try {
+                                    Log.i(TAG, "timeout=" + syncTask.getTimeout());
+                                    if (syncTask.getTimeout() != 0)
+                                        future.get(syncTask.getTimeout(), TimeUnit.MILLISECONDS);
+                                    else
+                                        future.get();
+                                } catch (Exception e) {
+                                    if (e instanceof TimeoutException) {
+                                        Log.w(TAG, "Timeout, canceling task: " + syncTask.getClass().getSimpleName());
+                                        syncTask.dispose();
 
-                                // Interrupt and stop the underlying task after 100ms
-                                CommonUtils.sleep(500);
-                                future.cancel(true);
-                            } else {
-                                // The may be the exception thrown by the underlying task
-                                e.printStackTrace();
+                                        // Interrupt and stop the underlying task after 100ms
+                                        CommonUtils.sleep(500);
+                                        future.cancel(true);
+                                    } else {
+                                        // The may be the exception thrown by the underlying task
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                taskRunningStatus.get(syncTask.getClass()).setRunning(false);
+                            },
+                            Throwable::printStackTrace,
+                            () -> {
                             }
-                        }
-
-                        taskRunningStatus.get(syncTask.getClass()).setRunning(false);
-                    },
-                    Throwable::printStackTrace,
-                    () -> {}
-                ));
+                    ));
         }
     }
 
